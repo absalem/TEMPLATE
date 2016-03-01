6be9dde3283f6b7bbd305cbfe5f62ce6e2c68e78
Christian Weingärtner <cweingaertner@menadwork.com>
2016-03-01 18:40:40 +0100

    update

---
+++ src/scripts/main.js
@@ -0,0 +1,11 @@
+// Passing a named function instead of an anonymous function.
+ 
+function init(jQuery) {
+    // Code to run when the document is ready.
+}
+
+ 
+$(document).ready(init);
+// or:
+$(window).load(init);
+
